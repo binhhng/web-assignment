0830ae027618ede7a81804191303d4879f95e57d
binhhng <64148429+binhhng@users.noreply.github.com>
2021-11-29 15:30:27 +0700

    blog

---
+++ blog-1.php
@@ -0,0 +1,71 @@
+<?php
+ob_start();
+// include header.php file
+include ('header.php');
+?>
+
+<section id="product" class="py-3">
+    <div div class="container">
+        <div class="row">
+            <h1>Đánh giá nhanh Xiaomi Redmi Note 7: rất đáng tiền</h1>
+            <br/><br/><br/><br/>
+            <img src="./assets/blog/blog-1.jpg" alt="cart image" class="card-img-top">
+            <div style="font-size:150%">
+                <br/>
+                <p>
+                    Redmi Note 7 khá là thú vị, nó là một con điển hình của Xiaomi với cấu hình tốt + giá tốt. Mình cũng hơi giật mình thấy một chiếc điện thoại chạy Snapdragon 660, camera 48 megapixel mà có giá bán chỉ 3,6 triệu đồng mà thôi (bản RAM 4GB + bộ nhớ trong 64GB thì đắt hơn tí xíu, nhưng vẫn ngon). Đây là một số chia sẻ nhanh của mình về con Redmi Note 7 này sau một thời gian sử dụng để anh em nào tính mua có thể tham khảo.
+                    <ul><b>Điểm mạnh :</b>
+                        <li>Hiệu năng rất tốt, giá cũng tốt</li>
+                        <li>Camera hoạt động ổn</li>
+                        <li>Pin trâu</li>
+                        <li>Cử chỉ cảm ứng full màn hình tốt nhất trong thế giới Android</li>
+                    </ul>
+                    <ul><b>Điểm yếu:</b>
+                        <li>Khu vực giọt nước của màn hình có vẻ tối hơn so với phần còn lại của màn hình</li>
+                        <li>MIUI 10 đôi khi không hiện thông báo (?)</li>
+                        <li>Máy ảnh chụp màu hơi nhạt vào buổi tối, đẩy khử noise khá mạnh tay</li>
+                    </ul>
+                    Chấm điểm chung: 7/10 <br/><br/>
+                    <b>Thiết kế: chưa phải xuất sắc nhưng ổn</b><br/>
+                    Nếu như bạn muốn một cái máy đẹp lung linh, viền mỏng thật mỏng thì Redmi Note 7 không phải dành cho bạn, nhưng nếu bạn nhìn thử thiết kế của nó so với những con điện thoại khác giá dưới 4 triệu thì bạn không thể đòi hỏi hơn. Cách mà Xiaomi làm ra Redmi Note 7 nhìn rất ổn, vừa đủ hài hòa, vừa đủ mỏng để bạn có thể không cảm thấy nó xấu.<br/>
+                    Redmi Note 7 dùng màn hình giọt nước, nó không phải là vấn đề và cũng không làm bạn khó chịu gì cả. Chỉ khi xem video hay chạy app muốn bung thành full screen thì nhìn hơi lạ một chút nhưng cũng không phải là vấn đề lớn.<br/><br/>
+                    <img src="./assets/blog/blog-1-1.jpg" alt="cart image" class="card-img-top"><br/><br/>
+                    Phần còn lại của máy được hoàn thiện tốt, không phải như kiểu sang chảnh của Mi Mix nhưng vẫn mang lại cảm giác chắc chắn, hiện đại và cầm trong tay rất thích thú.<br/>
+                    Cái đẹp nhất của Redmi Note 7 là mặt lưng. Mình đã từng được cầm bản Redmi Note 7 màu xanh dương, đẹp và ấn tượng lắm, rất hợp cho bạn nào thích cá tính, thích sự nổi bật. Bản mình xài để test thì chỉ là màu đen nên nhìn khá bình thường, chung chung, có cái phủ kính khắp mặt sau thì làm máy trông sang hơn. Nếu được thì bạn nên kiếm mua màu xanh nhé, có điều nhớ mua sớm hoặc đặt cọc vì mình nghe mấy bạn bán hàng ở Mi Store bảo là màu xanh vừa về phát là hết ngay.<br/><br/>
+                    <img src="./assets/blog/blog-1-2.jpg" alt="cart image" class="card-img-top"><br/><br/>
+                    <b>Màn hình</b><br/>
+                    Màn hình này ok, dùng được, độ phân giải đủ cao để làm cho mọi thứ trở nên mịn màn và nhìn ngoài nắng không bị phản chiếu mạnh. Nó cũng giống như màn hình của nhiều con smartphone khác dưới 5 triệu thôi chứ cũng không có gì đặc biệt xuất sắc.<br/>
+                    Nhưng có điều mình thấy xung quanh khu vực tai thỏ dường như có ánh sáng bị rò rỉ ra, nhìn nó đen hơn so với phần còn lại của màn hình. Mình sợ máy mình lỗi nên có check thêm một vài trang khác thì cũng bị tình trạng giống như mình. Khi sử dụng bình thường thì bạn ít khi để ý tới nó vì phần này không nằm trong vùng mà mắt sẽ nhìn vào, chỉ khi bạn cố tình chú ý thì mới thấy rõ.<br/><br/>
+                    <img src="./assets/blog/blog-1-3.jpg" alt="cart image" class="card-img-top"><br/><br/>
+                    <b>Hiệu năng: cái đáng giá nhất</b><br/>
+                    Máy chạy nhanh, mượt, sử dụng lâu không bị nóng, làm tất cả mọi việc ngon lành chính là điểm mạnh nhất của Redmi Note 7 đối với mình. Bạn dùng nó mà cứ tưởng như đang xài mấy con flagship. Nói cho ngay thì hiệu năng của Redmi Note 7 chạy chip Snapdragon 660 ngang ngang với mấy con flagship chạy Snapdragon 821 của 2-3 năm trước chứ kêu so với mấy con Snap 835, 855 thì chắc chắn không thể nào bằng được, nhưng như vậy cũng là rất ổn trong tầm tiền của nó.<br/>
+                    Một điều bạn cần biết là đa số app Android, kể cả game, đều phải thiết kế để dùng với rất nhiều đời khác nhau nên dù bạn chơi game kiểu PUBG hay chỉ đơn giản là một app ghi chú đơn giản thì đều đáp ứng được hết. Mình đoán rằng con Redmi Note 7 sẽ còn tiếp tục dùng ổn trong tầm 2-3 năm tới, vừa đủ thời gian để bạn chuyển sang dùng một cái máy khác.<br/><br/>
+                    <img src="./assets/blog/blog-1-4.jpg" alt="cart image" class="card-img-top"><br/><br/>
+                    Xiaomi đã cho thấy rằng bạn bỏ ít tiền thì bạn vẫn xứng đáng được trải nghiệm tốt, và nếu bạn chưa từng dùng điện thoại tầm trung thì Redmi Note 7 cũng sẽ thay đổi suy nghĩ của bạn về những dòng máy tựa tựa như thế này. Ngoài Redmi Note 7, chỉ có một chiếc điện thoại khác trong tầm giá dưới 4-5 triệu mà mình cảm thấy hài lòng và ấn tượng về hiệu năng, đó là Vsmart Active 1.<br/>
+                    Ngoài chuyện phần cứng ổn, giao diện MIUI 10 còn được tối ưu cho đủ loại thiết bị từ tầm trung cho đến cao cấp nên bạn không cảm thấy đứng hay giật trong quá trình sử dụng.<br/><br/>
+                    <b>MIUI</b><br/>
+                    Mình đã từng nói khá nhiều về tính năng hay của MIUI 10, mời anh em tham khảo ở bài: 5 tính năng hay nhất của MIUI 10 trên máy Xiaomi. Trong những cái hay nhất của MIUI thì mình đánh giá cao tính năng vuốt đa nhiệm của nó. Nó giúp khai thác triệt để thiết kế viền mỏng của những chiếc điện thoại như Redmi Note 7 vì nhìn máy đẹp hơn, đồng thời không gian có thể dùng để hiển thị nội dung cũng lớn hơn nhiều.<br/>
+                    MIUI cũng có nhiều chục tính năng nhỏ lớn khác nhau phù hợp cho bạn nào thích vọc điện thoại. Từ việc dọn dẹp máy cho đến chỉnh theme, bạn đều có thể can thiệp và điều chỉnh chiếc smartphone cho đúng với sở thích của mình. Hồi xưa mình cũng hay nghịch mấy cái nhỏ nhỏ này lắm, giờ thì hết rồi cứ để mặc định dùng thôi, và may mắn là đa số những thứ mặc định của MIUI 10 đều tốt.<br/><br/>
+                    <img src="./assets/blog/blog-1-5.jpg" alt="cart image" class="card-img-top"><br/><br/>
+                    Có một cái mình chưa rõ vì sao, đó là không phải app nào cũng hiển thị thông báo ra màn hình khóa của MIUI 10. Có vẻ như nó có thuật toán gì tối ưu riêng cho việc hiện thông báo hả anh em? Nhiều khi bị miss thông báo mà không để ý, phải vuốt notification center xuống mới thấy.<br/><br/>
+                    <b>Camera: vừa đủ dùng</b><br/>
+                    Mình sẽ nhờ anh em camera test kĩ hơn về con này, ở đây mình chia sẻ nhanh vài trải nghiệm của mình. Khi chụp đủ sáng, hình ảnh từ camera của Redmi Note 7 sắc nét, màu hơi nhạt một tí so với ngoài đời nhưng không ảnh hưởng nhiều, và hiệu ứng chiều sâu khi chụp các chủ thể gần (hoặc chụp chân dung) rất ấn tượng. Khi chụp thiếu sáng, ảnh ít noise, giữa được màu tốt nhưng cũng có xu hướng hơi nhạt một tí.<br/>
+                    Diểm chung trong các ảnh chụp full auto bằng Redmi Note 7 đó là thuật toán của Xiaomi xóa noise khá mạnh tay, kể cả khi đủ sáng, nên nếu zoom gần lại thì sẽ thấy rằng chủ thể không quá nét và vùng da bị "cà" mạnh. Nó làm cho ảnh trông khá bệt. Nếu để up trên mạng xã hội coi chơi thì không sao, nhưng đem lên làm hình nền máy tính, hiển thị ảnh trên TV cỡ lớn... thì sẽ thấy ngay nhược điểm này.<br/>
+                    Có cái là Redmi Note 7 chụp ảnh HDR rất tốt cho một cái điện thoại dưới 5 triệu. Mình thử đứng ngược hướng với mặt trời lặn chụp, thường thì sẽ nó ra ảnh mặt trời quá sáng và phần còn lại tối hẳn, nhưng không, Redmi Note 7 giữ được màu sắc và chi tiết của các căn nhà bên dưới gần như nguyên vẹn. Đánh giá cao điểm này.<br/><br/>
+                    <img src="./assets/blog/blog-1-6.jpg" alt="cart image" class="card-img-top"><br/><br/>
+                    <b>Pin: 1 ngày làm việc, đi chơi dư sức</b><br/>
+                    Giả sử 7h sáng bạn rút điện ra khỏi chiếc Note 7, bạn cầm nó đi làm (trong lúc ngồi ở công ty dùng Wi-Fi thay vì mạng di động) rồi sau đó lúc tan ca còn tiếp tục đi chơi với bạn bè thêm nữa thì con Note 7 vẫn có thể chịu được ngon lành. Thậm chí nếu tối hôm đó bạn quên cắm sạc thì máy vẫn trụ được tới sáng hoặc trưa ngày hôm sau.<br/>
+                    Trong một tình huống khác, nếu dùng 3G, 4G phần lớn thời gian thì máy vẫn có thể dùng được từ sáng tới tối, ví dụ như khi bạn đi du lịch chẳng hạn. Pin 4000mAh xuất hiện trên con Redmi Note 7 phải có lý do chính đáng chứ.<br/><br/>
+                    <b>Chốt lại</b><br/>
+                    Redmi Note 7 là một cái điện thoại đáng tiền và nên mua nếu bạn có ngân sách tầm 5 triệu đổ lại, muốn kiếm một cái điện thoại ngon lành để dùng trong 2-3 năm nữa. Hiệu năng rất tốt, thiết kế hiện đại, màu lưng xanh đẹp và pin trâu là những điểm mạnh nhất của con Redmi Note 7.<br/><br/>
+                </p>
+            </div>            
+        </div>
+    </div>
+</section>
+
+
+<?php
+// include footer.php file
+include ('footer.php');
+?>
--- blog_list.php
+++ blog_list.php
@@ -4,9 +4,39 @@ ob_start();
 include ('header.php');
 ?>
 
-<?php
-echo "About us"
-?>
+<section id="blogs">
+    <div class="container py-4">
+        <h4 class="font-rubik font-size-20">Latest Blogs</h4>
+        <hr>
+
+        <div class="owl-carousel owl-theme">
+        <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Xiaomi Redmi Note 7</h5>
+                    <img src="./assets/blog/blog-1.jpg" alt="cart image" class="card-img-top" width="100" height="150">
+                    <p class="card-text font-size-14 text-black-50 py-1">Đánh giá nhanh Xiaomi Redmi Note 7: rất đáng tiền</p>
+                    <a href="blog-1.php" class="color-second text-left">More details</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Samsung Galaxy S7</h5>
+                    <img src="./assets/blog/blog-2.jpg" alt="cart image" class="card-img-top" width="100" height="150">
+                    <p class="card-text font-size-14 text-black-50 py-1">Trên tay Samsung Galaxy S7: nhỏ gọn, rất mát và rất mạnh.</p>
+                    <a href="blog-2.php" class="color-second text-left">More details</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Apple iPhone 7</h5>
+                    <img src="./assets/blog/blog-3.jpg" alt="cart image" class="card-img-top" width="100" height="150">
+                    <p class="card-text font-size-14 text-black-50 py-1">Trên tay iPhone 7 hồng 256GB: máy đẹp hơn, màn hình đẹp hơn, cam to hơn...</p>
+                    <a href="blog-3.php" class="color-second text-left">More details</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php
 // include footer.php file
